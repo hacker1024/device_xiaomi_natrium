6a687634362ddb5cc7f4ede3c3e4f04f9081fa9d
Hlcpereira <pereirah2001@gmail.com>
2018-04-08 10:01:29 -0700

    natrium: AOSP BringUp

--- aosp_natrium.mk
+++ aosp_natrium.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/xiaomi/natrium/full_natrium.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_natrium
+PRODUCT_NAME := aosp_natrium
 PRODUCT_DEVICE := natrium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 5s Plus
