a80671d0b91c4c0314c90d92a3b97a81399e0fe2
hacker1024 <joshleivenzon@outlook.com>
2018-05-12 20:57:13 +1000

    Changes for RR

---
+++ rr.mk
@@ -0,0 +1,18 @@
+$(call inherit-product, device/xiaomi/natrium/full_natrium.mk)
+
+# Inherit some common RR stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+
+PRODUCT_NAME := rr_natrium
+PRODUCT_DEVICE := natrium
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 5s Plus
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="natrium" \
+    PRODUCT_NAME="natrium" \
+    BUILD_FINGERPRINT="Xiaomi/natrium/natrium:6.0.1/MXB48T/V8.2.4.0.MBGCNDL:user/release-keys" \
+    PRIVATE_BUILD_DESC="natrium-user 6.0.1 MXB48T V8.2.4.0.MBGCNDL release-keys"
